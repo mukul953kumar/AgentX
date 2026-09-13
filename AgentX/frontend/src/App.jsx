d889a604bb639619210d04fbea7d8e661c2cfffe
mukul <mukulkumar953622@gmail.com>
2026-09-13 15:49:09 +0530

    impliment Google authentication and Tailwind CSS

--- AgentX/frontend/src/App.jsx
+++ AgentX/frontend/src/App.jsx
@@ -1,32 +1,14 @@
-import { signInWithPopup } from 'firebase/auth'
+
 import React from 'react'
-import { auth, googleProvider } from '../utils/firebase.js'
-import api from '../utils/axios.js';
+import Home from './pages/Home.jsx';
 
 
 function App() {
 
-    const handleLogin = async (token) => {
-        try {
-            const { data } = await api.post("/auth/login", { token })
-            console.log(data)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    const googleLogin = async () => {
-        const data = await signInWithPopup(auth, googleProvider)
-        const token = await data.user.getIdToken()
-        console.log(token)
-        await handleLogin(token)
-        console.log(data)
-    }
     return (
-        <div className='w-full h-screen bg-black flex items-center justify-center'>
-            <button className='w-50 h-24 bg-white rounded-3xl' onClick={googleLogin}>
-                Continue with Google
-            </button>
-        </div>
+        <>
+        <Home/>
+        </>
     )
 }
 
