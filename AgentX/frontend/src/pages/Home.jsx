d7623c76d32ab6bf48dfa9256a7ca3fcb450e9fe
mukul <mukulkumar953622@gmail.com>
2026-09-20 16:22:16 +0530

    initialize frontend application with layout components

--- AgentX/frontend/src/pages/Home.jsx
+++ AgentX/frontend/src/pages/Home.jsx
@@ -4,6 +4,9 @@ import api from '../../utils/axios.js'
 import { auth, googleProvider } from '../../utils/firebase.js'
 import { FcGoogle } from "react-icons/fc";
 import { useDispatch, useSelector } from 'react-redux';
+import SideBar from '../components/SideBar.jsx';
+import ChatArea from '../components/ChatArea.jsx';
+import Artifact from '../components/Artifact.jsx';
 
 
 
@@ -31,6 +34,14 @@ function Home() {
     return (
         <div className=' h-screen flex bg-[#0d0f14] text-white overflow-hidden'>
 
+
+        <SideBar/>
+        <ChatArea/>
+        <Artifact/>
+
+
+
+                
             {!userData && <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/60 backdrop-blur-sm'>
                 <div className='w-[340px] bg-[#13151c] border border-white/[0.08] rounded-2xl p-7 flex flex-col gap-5'>
 
